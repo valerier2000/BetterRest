a688765c9879fe0f31f3cf1e403ce8829453a0cc
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2024-04-08 15:56:01 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Valeriia Rohatynska on 08/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
